e9c91c6175ebeb953fe088b986fd71d1e56af49e
Selva81294 <116962033+Selva81294@users.noreply.github.com>
2023-04-25 19:21:19 +0530

    Update task.js

--- task.js
+++ task.js
@@ -1,59 +1,271 @@
-//Get all the countries from the Asia continent /region using the Filter function
-/* let request = new XMLHttpRequest();
-request.open("GET","https://restcountries.com/v3.1/all");
-request.send();
-request.onload =function(){
-    let data = request.response
-    let result = JSON.parse(data);
-    let res = result.filter((ele)=>ele.continents=="Asia")
-    console.log(res);
-    let res1 = res.map((ele)=>ele.name.common)
-    console.log(res1);
-}  */
+//1. Anonymous & IIFE Function
+//a. Print odd numbers in an array
+//Anonymous Function
+/* let arr = [12,13,14,15,16,17,18,19,20];
+let result = [];
+let odd = function(arr) {
+    for (i=0;i<arr.length;i++){
+        if (arr[i]%2 != 0)
+        {
+            result.push(arr[i]);
+        }
+    }
+    return result;
+}
+console.log(odd(arr)); */
 
-//Get all the countries with a population of less than 2 lakhs using Filter function
-/* let request = new XMLHttpRequest();
-request.open("GET","https://restcountries.com/v3.1/all");
-request.send();
-request.onload =function(){
-    let data = request.response
-    let result = JSON.parse(data);
-    let res = result.filter((ele)=>ele.population<200000)
-    //console.log(res);
-    let res1 = res.map((ele)=>ele.name.common)
-    console.log(res1);
-}  */
+//IIFE
+/* (function odd(arr) {
+    let result = [];
+    for (i=0;i<arr.length;i++){
+        if (arr[i]%2 != 0)
+        {
+            result.push(arr[i]);
+        }
+    }
+    console.log(result);
+})([2,3,4,5,6,7,8,9]) */
 
-//Print the following details name, capital, flag using forEach function
-/* let request = new XMLHttpRequest();
-request.open("GET","https://restcountries.com/v3.1/all");
-request.send();
-request.onload =function(){
-    let data = request.response
-    let result = JSON.parse(data);
-    let res = result.forEach((ele)=>{console.log(`${ele.name.common} ${ele.capital} ${ele.flag}`)})
-}  */
+//Convert all the strings to title caps in a string array
+//Anonymous Function
+/* let titlecaps = function(str) {
+    let sentence = str.toLowerCase().split(" ");
+    for(i=0;i<sentence.length;i++)
+    {
+        sentence[i] = sentence[i][0].toUpperCase()+sentence[i].slice(1)
+    }
+    return sentence.join(" ")
+}
+console.log(titlecaps("guvi is best platform to learn")); */
 
-//Print the total population of countries using reduce function
-/* let request = new XMLHttpRequest();
-request.open("GET","https://restcountries.com/v3.1/all");
-request.send();
-request.onload =function(){
-    let data = request.response
-    let result = JSON.parse(data);
-    let res = result.reduce((acc,cv)=>acc+cv.population,0)
-    console.log(res);
-} */
+//IIFE
+/* (function titlecaps(str) {
+    let sentence = str.toLowerCase().split(" ");
+    for(i=0;i<sentence.length;i++)
+    {
+        sentence[i] = sentence[i][0].toUpperCase()+sentence[i].slice(1)
+    }
+    console.log(sentence.join(" "));
+})("guvi is best platform to learn"); */
 
-//Print the country which uses US Dollars as currency
-let request = new XMLHttpRequest();
-request.open("GET","https://restcountries.com/v3.1/all");
-request.send();
-request.onload =function(){
-    let data = request.response
-    let result = JSON.parse(data);
-    let res = result.filter((ele)=>ele?.currencies?.USD).map((value) => value.name.common);
-    console.log(res)
-}   
+// Sum of all number in an array
+//Anonymous Function
+/* let sum = function(arr){
+    let b = 0;
+    for(i=0;i<arr.length;i++)
+    {
+    b=b+arr[i];
+    }
+    return b
+}
+let b = [5,5,5,5];
+console.log(sum(b)); */
 
+//IIFE
+/* (function(arr){
+    let b = 0;
+    for(i=0;i<arr.length;i++)
+    {
+    b=b+arr[i];
+    }
+    console.log(b);
+})([1,2,3,4]) */ 
 
+//Return all the prime numbers in an array
+// Annonymous function
+/* var primenum=function(arr){
+    arr = arr.filter((number) => {
+    for (var i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) return false;
+    }
+    return true;
+    });
+   console.log(arr);
+    }
+    primenum([1,2,3,4,5,6,7,8]) */
+
+//IIFE
+/* (  
+    function(arr){
+    arr = arr.filter((number) => {
+    for (var i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) return false;
+    }
+    return true;
+    });
+     console.log(arr);
+    }
+    )([1,2,3,4,5,6,7,8]) */
+
+//Return all Palindrome in an array
+//Anonymous function
+/* let ispalindrome = function (a){
+    a = a.split('').reverse().join('');
+    return a;
+}
+let palindrome = function (arr){
+    let result = [];
+    for (i=0;i<arr.length;i++){
+        if(ispalindrome(arr[i]) === arr[i]){
+            result.push(arr[i]);
+        }
+    }
+    return result;
+}
+let arr = [ "madam", "car", "ada", "racecar", "cool"]
+console.log(palindrome(arr)); */
+
+//IIFE Function
+/* function ispalindrome(a){
+    a = a.split('').reverse().join('');
+    return a;
+}
+(function palindrome(arr){
+    let result = [];
+    for (i=0;i<arr.length;i++){
+        if(ispalindrome(arr[i]) === arr[i]){
+            result.push(arr[i]);
+        }
+    }
+    console.log(result);
+})([ "madam", "car", "ada", "racecar", "cool"]) */
+
+//print median of two sorted same arrays
+//Anonymous function
+/* let median = function(arr){
+    let len = arr.length;
+    let mid = Math.floor(len/2);
+    if(len % 2 != 0)
+    {
+        return arr[mid];
+    }
+    else {
+        return (arr[mid-1] + arr[mid]) / 2;
+    }
+}
+let arr1 = [3,2,5];
+let arr2 = [10,13,11];
+let arr = [...arr1,...arr2];
+let arrsort = arr.sort(function(a,b) {return a-b});
+console.log(arrsort);
+console.log(median(arrsort)); */
+
+//remove duplicates from an Array
+/* function removeDuplicates(arr) {
+    let result = [];
+    let arrsort = arr.sort()
+    for(i=0;i<arr.length;i++)
+    {
+        if(arrsort[i] !== arr[i+1]){
+            result.push(arrsort[i]);
+        }
+    }
+    return result
+}
+let a = [2,3,2,4,68,32,23,4,32]
+console.log(removeDuplicates(a));
+let b = removeDuplicates(a).sort(function(a,b){return a-b})
+console.log(b) */
+
+//Anonymous function
+/* let removeDuplicates = function (arr) {
+    let result = [];
+    let arrsort = arr.sort()
+    for(i=0;i<arr.length;i++)
+    {
+        if(arrsort[i] !== arr[i+1]){
+            result.push(arrsort[i]);
+        }
+    }
+    return result
+}
+let a = [2,3,2,4,68,32,23,4,32]
+console.log(removeDuplicates(a));
+let b = removeDuplicates(a).sort(function(a,b){return a-b})
+console.log(b) */
+
+//rotate an array by k times
+//Anonymous Function
+/* let rotate = function(arr, k){
+    for(i=0;i<k;i++){
+        arr.unshift(arr.pop());
+    }
+    return arr;
+}
+console.log(rotate([1,2,3,4,5],4)); */
+
+//IIFE
+/* (function rotate(arr, k){
+    for(i=0;i<k;i++){
+        arr.unshift(arr.pop());
+    }
+    console.log(arr);
+})([1,2,3,4,5],3); */
+
+//Arrow Function
+//print odd numbers
+/* let odd = (arr)=>{
+    let result = [];
+    for (i=0;i<arr.length;i++){
+        if (arr[i]%2 != 0)
+        {
+            result.push(arr[i]);
+        }
+    }
+    return result;
+}
+let arr = [2,3,4,5,6,7,8,9];
+console.log(odd(arr)); */
+
+//Convert all the strings to title caps in a string array
+/* let titlecaps = (str)=> {
+    let sentence = str.toLowerCase().split(" ");
+    for(i=0;i<sentence.length;i++)
+    {
+        sentence[i] = sentence[i][0].toUpperCase()+sentence[i].slice(1)
+    }
+    return sentence.join(" ")
+}
+console.log(titlecaps("guvi is best platform to learn")); */
+
+//Sum of all numbers in an array
+// let sum = (arr) =>{
+//     let b = 0;
+//     for(i=0;i<arr.length;i++)
+//     {
+//     b=b+arr[i];
+//     }
+//     return b
+// }
+// let b = [5,5,5,5];
+// console.log(sum(b));
+
+//Return all the prime numbers in an array
+//Arrow function
+/* var prime = (arr) => {
+    arr = arr.filter((number) => {
+    for (var i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) return false;
+    }
+    return true;
+    });
+    console.log(arr);
+}
+    prime([1,2,3,4,5,6,7,8]) */
+
+//Return all the palindromes in an array
+/* let ispalindrome = (a)=>{
+    a = a.split('').reverse().join('');
+    return a;
+}
+let palindrome = (arr)=>{
+    let result = [];
+     for (i=0;i<arr.length;i++){
+        if(ispalindrome(arr[i]) === arr[i]){
+            result.push(arr[i]);
+        }
+    } 
+    return result;
+}
+let arr = [ "madam", "car", "ada", "racecar", "cool"]
+console.log(palindrome(arr)); */
